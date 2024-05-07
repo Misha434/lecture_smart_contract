47aa826b22c9569f7dc8b15f6789f3b5e9649e1d
Misha434 <missymouse434@gmail.com>
2024-05-07 21:42:29 +0900

    update

---
+++ OnlyOwnerMintWithModifier.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+
+contract OnlyOwnerMintWithModifier is ERC721 {
+    
+    /**
+    * @dev
+    * - このコントラクトをデプロイしたアドレス用変数 owner
+    */
+    address public owner;
+
+    constructor() ERC721("OnlyOwnerMintWithModifier", "OWNERMOD"){
+        owner = _msgSender();
+    }
+
+    /**
+    * @dev
+    * - このコントラクトをデプロイしたアドレスだけに制御する modifier
+    */
+    modifier onlyOwner {
+        require(owner == _msgSender(), "Caller is not the owner");
+        _;
+    }
+
+    /**
+    * @dev
+    * - このコントラクトをデプロイしたアドレスだけがmint可能 onlyOwner
+    * - nftMint関数の実行アドレスにtokenIdを紐付け
+    */
+    function nftMnt(uint256 tokenId) public onlyOwner {
+        _mint(_msgSender(), tokenId);
+    }
+}
